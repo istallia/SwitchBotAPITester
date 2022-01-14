7a2229f0c5377f0e41b35256f75d1a34a27c8cfe
Istallia <55042065+istallia@users.noreply.github.com>
2022-01-14 21:19:24 +0900

    [Modify] コメントを自己流に改変

--- app/index.js
+++ app/index.js
@@ -1,18 +1,19 @@
-const electron = require('electron');
-const app = electron.app;
+/* --- ライブラリ(?)の読み込み --- */
+const electron      = require('electron');
+const app           = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow = null;
+
+/* --- ウィンドウを開く --- */
+let main_window = null;
 app.on('ready', () => {
-	// mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-	mainWindow = new BrowserWindow({width: 1280, height: 720});
-	// Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
-	mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-	// ChromiumのDevツールを開く
-	mainWindow.webContents.openDevTools();
-
-	mainWindow.on('closed', function() {
-		mainWindow = null;
+	/* main_windowを作成(windowの大きさや、Kioskモードにするかどうかなどもここで定義できる) */
+	main_window = new BrowserWindow({width: 1280, height: 720});
+	/* Electronに表示するhtmlを絶対パスで指定(相対パスだと動かない) */
+	main_window.loadURL('file://' + __dirname + '/index.html');
+	/* ChromiumのDevツールを開く */
+	main_window.webContents.openDevTools();
+	main_window.on('closed', function() {
+		main_window = null;
 	});
 });
