b4ffdd68c0e71cfb85a7fa2034c6ed1d2db11528
Istallia <55042065+istallia@users.noreply.github.com>
2022-01-15 22:32:23 +0900

    [Add] メインエリアをサイドバーで切り替えられるように

--- app/tester.js
+++ app/tester.js
@@ -1,3 +1,24 @@
+/* --- メインエリアの切り替え --- */
+const switchMainArea = event => {
+	/* ハッシュがなければ解散 */
+	const url = new URL(event.newURL);
+	console.log(url.hash);
+	if (url.hash.length < 1) url.hash = '#section-token';
+	/* 現在の要素を非表示 */
+	document.querySelector('.main.selected'   ).classList.remove('selected');
+	document.querySelector('.content.selected').classList.remove('selected');
+	/* 次の要素を表示 */
+	const id   = url.hash.slice(1);
+	const area = document.getElementById(id);
+	if (area) {
+		area.classList.add('selected');
+		document.querySelector(`.content > a[href="#${id}"]`).parentNode.classList.add('selected');
+	}
+};
+window.addEventListener('hashchange', switchMainArea);
+
+
+
 /* --- インスペクターエリアの出し入れ --- */
 document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector('.inspector-opener').addEventListener('click', event => {
