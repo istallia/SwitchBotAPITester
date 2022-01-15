70468e803d881ff28450674732b1153537e676f5
Istallia <55042065+istallia@users.noreply.github.com>
2022-01-15 16:17:21 +0900

    [Add] サイドバーとその基礎的なスタイルを作成

--- app/index.js
+++ app/index.js
@@ -7,13 +7,13 @@ const BrowserWindow = electron.BrowserWindow;
 /* --- ウィンドウを開く --- */
 let main_window = null;
 app.on('ready', () => {
-	/* main_windowを作成(windowの大きさや、Kioskモードにするかどうかなどもここで定義できる) */
-	main_window = new BrowserWindow({width: 1280, height: 720});
-	/* Electronに表示するhtmlを絶対パスで指定(相対パスだと動かない) */
+	/* ウィンドウを開く */
+	main_window = new BrowserWindow({width: 1280, height: 720, minWidth: 512, minHeight: 408});
 	main_window.loadURL('file://' + __dirname + '/index.html');
+	main_window.setMenu(null);
 	/* ChromiumのDevツールを開く */
 	main_window.webContents.openDevTools();
-	main_window.on('closed', function() {
+	main_window.on('closed', () => {
 		main_window = null;
 	});
 });
